d0cc649c2246c88ad6c6457836e663b4a339cf7d
Aleksejs Pridannikovs <alex.demolliens@gmail.com>
2020-02-10 15:15:40 +0100

    filter test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'cinema-app';
 
+  public defaultContry = 'all';
   public movies: any [] = [
     {
       title: 'Joker',
-      year: 2019
+      year: 2019,
+      contry: 'it'
     },
     {
       title: 'Rome',
-      year: 2003
+      year: 2003,
+      contry: 'it'
     },
     {
       title: 'Avengers: End Game',
-      year: 2019
+      year: 2019,
+      contry: 'us'
+    },
+    {
+      title: 'Green Book',
+      year: 2017,
+      contry: 'us'
     }
-  ]
+  ];
+
+  public toggleContry(): void {
+    this.defaultContry = (this.defaultContry === 'us') ? this.defaultContry = 'it' : this.defaultContry = 'us';
+
+    this.movies.forEach((movie: any) => {
+      movie.show = movie.contry === this.defaultContry;
+    });
+  }
 }
