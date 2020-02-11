9a072a53ffd88349e583f3d545c9523e00618fd3
Aleksejs Pridannikovs <alex.demolliens@gmail.com>
2020-02-11 10:09:57 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,67 +6,6 @@ import { Component, OnInit,  } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-  public title = 'cinema-app';
-
-  public defaultContry = 'all';
-  // public countries: Set<string> = new Set();
-  public countries: Map<string, any> = new Map<string, any>();
-  public movies: any [] = [
-    {
-      title: 'Italian Movie',
-      year: 1993,
-      contry: {
-        iso: 'it',
-        text: 'Italie'
-      }
-    },
-    {
-      title: 'Intouchable ',
-      year: 2011,
-      contry: {
-        iso: 'fr',
-        text: 'France'
-      }
-    },
-    {
-      title: 'Rec',
-      year: 2007,
-      contry: {
-        iso: 'es',
-        text: 'Spain'
-      }
-    },
-    {
-      title: 'Joker',
-      year: 2019,
-      contry: {
-        iso: 'us',
-        text: 'United States'
-      }
-    },
-    {
-      title: 'GreenBook',
-      year: 2017,
-      contry: {
-        iso: 'us',
-        text: 'United States'
-      }
-    }
-  ];
-
-  public toggleContry(): void {
-    this.defaultContry = (this.defaultContry === 'us') ? this.defaultContry = 'it' : this.defaultContry = 'us';
-    this.movies.forEach((movie: any) => {
-      movie.show = movie.contry.iso === this.defaultContry;
-    });
-  }
-
+export class AppComponent{
   public constructor() {}
-
-  public ngOnInit(): void {
-    this.movies.forEach(movie =>{
-      this.countries.set(movie.contry.iso, movie.contry);
-    });
-  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public title = 'cinema-app';
+  public defaultContry = 'all';
+  public countries: Map<string, any> = new Map<string, any>();
+
+  public movies: any [] = [
+    {
+      title: 'Italian Movie',
+      year: 1993,
+      contry: {
+        iso: 'it',
+        text: 'Italie'
+      }
+    },
+    {
+      title: 'Intouchable ',
+      year: 2011,
+      contry: {
+        iso: 'fr',
+        text: 'France'
+      }
+    },
+    {
+      title: 'Rec',
+      year: 2007,
+      contry: {
+        iso: 'es',
+        text: 'Spain'
+      }
+    },
+    {
+      title: 'Joker',
+      year: 2019,
+      contry: {
+        iso: 'us',
+        text: 'United States'
+      }
+    },
+    {
+      title: 'GreenBook',
+      year: 2017,
+      contry: {
+        iso: 'us',
+        text: 'United States'
+      }
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.movies.forEach(movie =>{
+      this.countries.set(movie.contry.iso, movie.contry);
+    });
   }
 
+  public toggleContry(): void {
+    this.defaultContry = (this.defaultContry === 'us') ? this.defaultContry = 'it' : this.defaultContry = 'us';
+    this.movies.forEach((movie: any) => {
+      movie.show = movie.contry.iso === this.defaultContry;
+    });
+  }
 }
