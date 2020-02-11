21af775a6eeb2d28f63b464083187706f07f9cd5
Aleksejs Pridannikovs <alex.demolliens@gmail.com>
2020-02-11 09:24:10 +0100

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit,  } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   public title = 'cinema-app';
 
   public defaultContry = 'all';
